fa474b9f1e03198490749bf9b8d08abc7825bf9b
jonkeyguan <jonkey.guan@gmail.com>
2022-02-28 12:08:09 +0800

    added drools g/w

--- src/main/java/com/sample/accounting/Application.java
+++ src/main/java/com/sample/accounting/Application.java
@@ -1,5 +1,6 @@
 package com.sample.accounting;
 
+import com.sample.accounting.gateway.RulesGateway;
 import com.sample.accounting.resource.AccountingResource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +14,18 @@ public class Application implements CommandLineRunner{
     @Autowired
     AccountingResource resource;
 
+    // @Autowired
+    // RulesGateway rulesGateway;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
     @Override
     public void run(String... args) throws Exception {
-        resource.accountingByProduct();
-        // resource.accountingByChannel();
+        // resource.accountingByProduct();
+        resource.accountingByChannel();
+        // rulesGateway.test();
     }
 
 }
