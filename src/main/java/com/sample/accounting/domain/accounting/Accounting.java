3f6605ea8328a8fbe40fb8b2d0942ea7ab64464e
jonkeyguan <jonkey.guan@gmail.com>
2022-02-28 15:19:04 +0800

    removed unused injected bean

--- src/main/java/com/sample/accounting/domain/accounting/Accounting.java
+++ src/main/java/com/sample/accounting/domain/accounting/Accounting.java
@@ -3,11 +3,6 @@ package com.sample.accounting.domain.accounting;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sample.accounting.domain.Buyer;
-import com.sample.accounting.domain.Channel;
-import com.sample.accounting.domain.Market;
-import com.sample.accounting.domain.workhours.WorkHours;
-import com.sample.accounting.domain.workhours.WorkHoursNoneBG;
 import com.sample.accounting.repository.AccountingRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,24 +20,6 @@ public class Accounting {
 
     @Autowired
     private AccountingRepository repository;
-
-    @Autowired
-    private WorkHours workHours;
-
-    @Autowired
-    private WorkHoursNoneBG workHoursNoneBG;
-
-    @Autowired
-    private AccountingMerge accountingMerge;
-
-    @Autowired
-    private Channel channel;
-
-    @Autowired
-    private Market market;
-
-    @Autowired
-    private Buyer buyer;
 
     public void accountingByProduct() {
         List<AccountingItem> items = repository.load();
